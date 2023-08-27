d302cafe97b2ea3189e27100776e93ea998ad0fe
bellavergara <bellaiivanavergara@gmail.com>
2023-08-27 02:11:12 -0300

    configuracion de taiilwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  purg: [
+    './src/**/*.(js,jsx,ts,tsx)'
+  ],
+  theme: {
+    extend: {}
+  },
+  variants: {},
+  plugins: []
+}
